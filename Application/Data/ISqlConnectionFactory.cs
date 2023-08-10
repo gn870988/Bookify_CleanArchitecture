c49f1dadce67f7268c312e002dedbdb85a3ef411
Vincent.yu <gn870988@gmail.com>
2023-08-10 14:35:54 +0800

    Finished application layer

---
+++ Application/Data/ISqlConnectionFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace Application.Data;
+
+public interface ISqlConnectionFactory
+{
+    IDbConnection CreateConnection();
+}
---
+++ Doamin/Apartments/ApartmentErrors.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Apartments;
+
+public static class ApartmentErrors
+{
+    public static Error NotFound = new(
+        "Property.Found",
+        "The property with the specified identifier was not found");
+}
